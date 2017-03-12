9d6b3630b0a74b2172baa344d665540dfc5dc6fe
JiangGUOGUO <mingpei2013@163.com>
2017-03-12 20:54:26 +0800

    update to test2

--- levelTest_Pei Ming.R
+++ levelTest_Pei Ming.R
@@ -9,3 +9,15 @@ findNum <- function(y,x,l){
   return(Num)
 }
 findNum(y,1,2)
+
+#1 hour
+
+
+raw <- read.delim("data/weather.txt",check.names = F, na.strings = ".")
+library(reshape2)
+newdata <- melt(raw, id.vars= names(raw)[1:3], variable.name = "day",value.name = "temp")
+newdata2 = na.omit(newdata)
+newdata3 <- dcast(newdata2,year+month+day~element)
+dif <- newdata3$tmax - newdata3$tmin
+
+#45 min
